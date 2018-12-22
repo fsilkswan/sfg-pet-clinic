082105da33bf3669e25caa2d9cecc18998df8d11
fseidenschwan <ferdinand@seidenschwan.de>
2018-12-22 22:46:27 +0100

    Added properties. Closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -7,15 +7,48 @@ public final class Owner
 {
     private static final long serialVersionUID = 1L;
 
+    private String   address;
+    private String   city;
     private Set<Pet> pets;
+    private String   telephone;
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
 
     public Set<Pet> getPets()
     {
         return pets;
     }
 
+    public String getTelephone()
+    {
+        return telephone;
+    }
+
+    public void setAddress(final String address)
+    {
+        this.address = address;
+    }
+
+    public void setCity(final String city)
+    {
+        this.city = city;
+    }
+
     public void setPets(final Set<Pet> pets)
     {
         this.pets = pets;
+    }
+
+    public void setTelephone(final String telephone)
+    {
+        this.telephone = telephone;
     }
 }
