b5418e1e444f8be8d14c6d55ac1784fcdc9ee97f
fseidenschwan <ferdinand@seidenschwan.de>
2019-01-03 00:49:21 +0100

    Implemented Map Service for Visits. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+@Service
+public class VisitMapService
+    extends AbstractMapService<Visit>
+    implements VisitService
+{
+    @Override
+    public Visit save(final Visit visit)
+    {
+        if( visit == null )
+        {
+            return null;
+        }
+
+        final Pet pet = visit.getPet();
+        if( pet == null || pet.getId() == null || pet.getOwner() == null || pet.getOwner().getId() == null )
+        {
+            throw new RuntimeException("Invalid Visit!");
+        }
+
+        return super.save(visit);
+    }
+}
