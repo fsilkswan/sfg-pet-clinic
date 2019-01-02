1e41638be177f0571bb7a75e9be337ad40e9cedb
fseidenschwan <ferdinand@seidenschwan.de>
2019-01-02 23:42:14 +0100

    Added Spring Data JPA service for Owners. Closes #40.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.sfgpetclinic.model.Owner;
@@ -7,4 +9,5 @@ import guru.springframework.sfgpetclinic.model.Owner;
 public interface OwnerRepository
     extends CrudRepository<Owner, Long>
 {
+    Optional<Owner> findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,70 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+@Service
+@Profile(value = { "springdatajpa" })
+public class OwnerSDJpaService
+    implements OwnerService
+{
+    private final OwnerRepository   ownerRepository;
+    private final PetRepository     petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(final OwnerRepository ownerRepository, final PetRepository petRepository,
+                             /**/ final PetTypeRepository petTypeRepository)
+    {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public void delete(final Owner entity)
+    {
+        ownerRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(final Long id)
+    {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<Owner> findAll()
+    {
+        final Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+
+        return owners;
+    }
+
+    @Override
+    public Owner findById(final Long id)
+    {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner findByLastName(final String lastName)
+    {
+        return ownerRepository.findByLastName(lastName).orElse(null);
+    }
+
+    @Override
+    public Owner save(final Owner entity)
+    {
+        return ownerRepository.save(entity);
+    }
+}
